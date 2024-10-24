45747cea7623854f4a0862f4f7c2246c2c804b19
bob-th <67490142+bob-th@users.noreply.github.com>
2024-10-24 18:51:57 -0400

    f

--- models/postModel.js
+++ models/postModel.js
@@ -2,37 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema
 //Schema defines the defines the structure of the documents saved to the collection
-const postSchema = new Schema({userId: {
-        type: String,
-        required: true
-    },
-    jobListingId: {
-        type: String,
-        required: true
-    },
-    userType: {
-        type: String,
-        enum: ['client', 'contractor'],
-        required: true
-    },
-    profile: {
-        name: {
-            required: true
-        },
-        bio: {
-            type: String,
-            default: ''
-        },
-        //idk how we want to do location
-        location: {
-            type: String,
-            required: true 
-        },
-        organization: {
-            type: String
-        }
-    },
-    
+const postSchema = new Schema({
     contractorId: { type: String, required},
     title: { type: String, required },
     description: { type: String, required },
