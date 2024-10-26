cd2b83cbb590b63f217069e4c500eed806b5be41
Peter Yoo <petersyoo23@gmail.com>
2024-10-26 05:18:33 -0400

    aszdasdqwe

--- app/src/Components/JS/JasonBackEndHelp.js
+++ app/src/Components/JS/JasonBackEndHelp.js
@@ -6,10 +6,10 @@ export default function searchQuery(searchResult, address, setSearchedResults){
    //jason implement here thanks 
 
 
-    setSearchedResults([{id:"", title:"Title", image:"", description:"Penis", review:""},
-        {id:"", title:"Title", image:"", description:"Penis", review:""},
-        {id:"", title:"Title", image:"", description:"Penis", review:""},
-        {id:"", title:"Title", image:"", description:"Penis", review:""}
+    setSearchedResults([{id:"", title:"qwe", image:"", description:"asdasd", review:""},
+        {id:"", title:"Titlasdadse", image:"", description:"asdasd", review:""},
+        {id:"", title:"zxc", image:"", description:"zxczxc", review:""},
+        {id:"", title:"asd", image:"", description:"qwe", review:""}
     ]); //make sure its an ARRAY
     //OR IT RETURN EMPTY OR ELSE CODE CRASH
 };
