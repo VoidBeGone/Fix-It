2b3c681cdd861c58337dfbb726962b609f636aab
Taemin Lee <taeminlee0601@gmail.com>
2024-10-26 03:52:57 -0400

    ahhhhh

--- app/src/Components/JS/JasonBackEndHelp2.js
+++ app/src/Components/JS/JasonBackEndHelp2.js
@@ -3,15 +3,14 @@
 
 
 export default function serviceQuery(someuserid, setSearchResults){
-    
-    setSearchResults([
-        {id:"", title:"Title", Description:"Description", Date:"2024-10-25", Progress:""},
-        {id:"", title:"PENIS", Description:"Description", Date:"2024-10-25", Progress:""},
-        {id:"", title:"Title", Description:"Description", Date:"2024-10-25", Progress:""},
-        {id:"", title:"Title", Description:"Description", Date:"2024-10-25", Progress:""},
-        {id:"", title:"Title", Description:"Description", Date:"2024-10-25", Progress:""},
-        {id:"", title:"Title", Description:"Description", Date:"2024-10-25", Progress:""},
-        {id:"", title:"Title", Description:"Description", Date:"2024-10-25", Progress:""}
-    ])
+    fetch('/api/me/jobs')
+        .then((jobs) => {
+            setSearchResults(jobs);
+        })
+        .catch((err) => {
+            console.error('Fetch error:', err);
+        }
+    );
+
     return;
 };
